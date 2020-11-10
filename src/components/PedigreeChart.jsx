9436eb9412a48acf713e554d9b3572e6c250f592
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2020-11-10 02:16:41 +0200

    finish dirty form

--- src/components/PedigreeChart.jsx
+++ src/components/PedigreeChart.jsx
@@ -1,14 +1,23 @@
 import React, {useEffect, useRef} from 'react';
+import {fabric} from 'fabric/dist/fabric'
+
 export default function PedigreeChart(props){
 
     let canvas;
 
     useEffect(() => {
-        
-        hi();
+        canvas = new fabric.Canvas('fabric-canvas');
+
+  var rect = new fabric.Rect({
+          width: 200, height: 100, left: 0, top: 50, angle: 30,
+          fill: 'rgba(255,0,0,0.5)'
+        });
+
+      canvas.add(rect);
+      console.log('added')
       });
 
 
-    return (<canvas  id="fabric-canvas" width="300" height="300" style={{backgroundColor: 'green'}}></canvas>
+    return (<canvas  id="fabric-canvas" width="300" height="300"></canvas>
     )
 }
