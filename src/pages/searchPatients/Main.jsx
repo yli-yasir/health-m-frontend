b2b40c8a859abde4975e16f38b01fb61efe9780a
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2020-12-20 16:17:44 +0200

    useAsync + mockend

--- src/pages/searchPatients/Main.jsx
+++ src/pages/searchPatients/Main.jsx
@@ -1,7 +1,19 @@
 import PatientSearchBar from "./partials/PatientSearchBar";
+import SearchResultsContrainer from "./partials/SearchResultsContainer";
+import {searchPatients} from '../../utils/mockAPIUtils';
+import {useAsync} from 'react-use';
 
 export default function SearchPatients(props) {
+  const query= props.location.search;
+
   return (
+    <React.Fragment>
     <PatientSearchBar/>
+    <SearchResultsContainer
+    results={}
+    loading={}
+    error={}
+     />
+    </React.Fragment>
   );
 }
