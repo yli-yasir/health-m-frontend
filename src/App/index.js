b304a1b28b9986da27d8dfea2ca2278ef247545d
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2021-08-29 21:05:29 +0300

    ci false, use HashRouter

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { LOGIN_PATH } from "./routePaths";
 import LoginPage from "../pages/Login";
 import GuardedApp from "./GuardedApp";
